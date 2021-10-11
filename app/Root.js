9cc03685f2892e046263c6ef315c34005665d1f6
PiyumiAnupama <96447@fhss.sjp.ac.lk>
2021-10-12 02:01:22 +0530

    create second UI

--- app/Root.js
+++ app/Root.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import LockScreenPasscode from '../app/screens/LockScreenPasscode';
+//import LockScreenPasscode from '../app/screens/LockScreenPasscode';
+import ApplicationLocked from './screens/ApplicationLocked';
 
 export default class Root extends Component {
   render() {
-    return <LockScreenPasscode />;
+    return <ApplicationLocked />;
   };
 }
---
+++ app/screens/ApplicationLocked.js
@@ -0,0 +1,91 @@
+import React, {Component, useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  SafeAreaView,
+  Image,
+} from 'react-native';
+
+export default class ApplicationLocked extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View
+          style={{
+            marginTop: 100,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View>
+            <Text style={styles.Text}> Maximum attempts reached</Text>
+          </View>
+          <View style={styles.image}>
+            <Image source={require('../assets/Image/Icon/LOCK.png')} />
+          </View>
+          <View>
+            <Text style={styles.Text2}>
+              To protect your information, access has been locked for 5 minutes.
+              Come back later and try again.
+            </Text>
+          </View>
+          <TouchableOpacity onPress={() => this}>
+            <View style={styles.Text3View}>
+              <Text style={styles.Text3}> Quit</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  Text: {
+    fontFamily: 'Roboto-Black',
+    fontSize: 25,
+    color: '#92969f',
+    letterSpacing: 0.34,
+    lineHeight: 25,
+  },
+
+  image: {
+    width: 32,
+    height: 35,
+    marginTop: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Text2: {
+    marginTop: 50,
+    fontFamily: 'Roboto-Black',
+    fontSize: 22,
+    color: '#92969f',
+    letterSpacing: 0.34,
+    lineHeight: 25,
+    justifyContent: 'flex-end',
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: 'center',
+  },
+  Text3View: {
+    backgroundColor: '#a6dde0',
+    width: 100,
+    height: 60,
+    marginTop: 300,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  Text3: {
+    fontFamily: 'Roboto-Black',
+    fontSize: 22,
+    marginLeft: 20,
+  },
+});
